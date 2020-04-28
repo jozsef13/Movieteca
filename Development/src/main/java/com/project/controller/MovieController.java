e80995b5ce334625da5e76cf1a9ad9db985f8942
Impalez <38750060+Impalez@users.noreply.github.com>
2020-04-28 23:01:00 +0300

    Update-v3
    -Movie Controller
    -Movie Service

--- Development/src/main/java/com/project/controller/MovieController.java
+++ Development/src/main/java/com/project/controller/MovieController.java
@@ -1,23 +1,116 @@
 package com.project.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.project.application.MovietecaApplication;
+import com.project.module.Cart;
 import com.project.module.Movie;
+import com.project.service.CartService;
 import com.project.service.MovieService;
 
+@RestController
 public class MovieController {
 
 	@Autowired
 	private MovieService movieService;
+	@Autowired
+	private CartController cartController;
+	@Autowired
+	private CartService cartService;
+
+	@PostMapping("/addMovie")
+	public ModelAndView addMovie(Movie movie) {
+		ModelAndView model = new ModelAndView("singlemovie");
+		movieService.addMovie(movie);
+		model.addObject(movie);
+		Cart cart = cartService.getCartById(MovietecaApplication.cartId);
+		if (cart != null) {
+			model.addObject(cart);
+		}
+		return model;
+	}
+
+	@GetMapping("/movies")
+	public ModelAndView getAllMovies() {
+		ModelAndView model = new ModelAndView("movies");
+		List<Movie> movies = new ArrayList<Movie>(movieService.getAllMovies());
+		model.addObject("movies", movies);
+		Cart cart = cartService.getCartById(MovietecaApplication.cartId);
+		if (cart != null) {
+			model.addObject(cart);
+		}
+		return model;
+	}
+
+	@GetMapping("/movies/{sortTypeName}")
+	public ModelAndView getMovieSortedByName(@PathVariable String sortTypeName) {
+		ModelAndView model = new ModelAndView("movies");
+		List<Movie> movies = movieService.getMovieSortedByName(sortTypeName);
+		model.addObject("movies", movies);
+		Cart cart = cartService.getCartById(MovietecaApplication.cartId);
+		if (cart != null) {
+			model.addObject(cart);
+		}
+
+		return model;
+	}
 
 	@GetMapping("/movie/{id}")
 	public ModelAndView getMovieById(@PathVariable int id) {
 		ModelAndView model = new ModelAndView("singlemovie");
 		Movie movie = movieService.getMovieById(id);
 		model.addObject(movie);
+		Cart cart = cartService.getCartById(MovietecaApplication.cartId);
+		if (cart != null) {
+			model.addObject(cart);
+		}
+		return model;
+	}
+
+	@GetMapping("/addToCart/Movie/{id}")
+	public ModelAndView addMovieToCart(@PathVariable int id, HttpServletRequest request) {
+		String type = request.getParameter("orderType");
+		Movie movie = movieService.getMovieById(id);
+		ModelAndView model = cartController.addMovieToCart(movie, type);
+		return model;
+	}
+	
+	@GetMapping("/movies/byPrice")
+	public ModelAndView getMoviesByPrice(HttpServletRequest request) {
+		ModelAndView model = new ModelAndView("movies");
+		String min = request.getParameter("minPrice");
+		String max = request.getParameter("maxPrice");
+		double minD = Double.parseDouble(min);
+		double maxD = Double.parseDouble(max);
+		List<Movie> sortedMovies = movieService.getMoviesByPrice(minD, maxD);
+		model.addObject("movies", sortedMovies);
+		Cart cart = cartService.getCartById(MovietecaApplication.cartId);
+		if (cart != null) {
+			model.addObject(cart);
+		}
+		return model ;
+	}
+	
+	@GetMapping("/movies/genre")
+	public ModelAndView getMoviesByGenre(HttpServletRequest request) {
+		ModelAndView model = new ModelAndView("movies");
+		String genre = request.getParameter("genre");
+		List<Movie> movies = movieService.getAllMoviesByGenre(genre);
+		model.addObject("movies", movies);
+		Cart cart = cartService.getCartById(MovietecaApplication.cartId);
+		if (cart != null) {
+			model.addObject(cart);
+		}
 		return model;
 	}
 }
--- Development/src/main/java/com/project/service/MovieService.java
+++ Development/src/main/java/com/project/service/MovieService.java
@@ -1,18 +1,101 @@
 package com.project.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.project.module.Movie;
 import com.project.repository.MovieRepository;
 
+@Service
 public class MovieService {
 
 	@Autowired
 	private MovieRepository movieRepo;
-
+	
+	public void addMovie(Movie movie) {
+		movieRepo.save(movie);
+	}
+	
+	public List<Movie> getAllMovies(){
+		return movieRepo.findAll();
+	}
+	
 	public Movie getMovieById(int movieId) {
 		return movieRepo.findById(movieId).orElseThrow(() -> new EntityNotFoundException());
+	}
+
+	public List<Movie> getAllMoviesByGenre(String genre) {
+		return movieRepo.findByGenre(genre);
+	}
+
+	public List<Movie> getMovieSortedByName(String sortTypeName) {
+		List<Movie> movies = new ArrayList<Movie>(getAllMovies());
+
+		if (sortTypeName.equals("aToZ")) {
+			Collections.sort(movies, new Comparator<Movie>() {
+				@Override
+				public int compare(Movie o1, Movie o2) {
+					return o1.getName().compareTo(o2.getName());
+				}
+			});
+		} else if (sortTypeName.equals("zToA")) {
+			Collections.sort(movies, new Comparator<Movie>() {
+				@Override
+				public int compare(Movie o1, Movie o2) {
+					return o1.getName().compareTo(o2.getName());
+				}
+			});
+			Collections.reverse(movies);
+		} else if (sortTypeName.equals("BuyPriceDescending")) {
+			Collections.sort(movies, new Comparator<Movie>() {
+				@Override
+				public int compare(Movie o1, Movie o2) {
+					return Double.compare(o1.getBuyPrice(), o2.getBuyPrice());
+				}
+			});
+			Collections.reverse(movies);
+		} else if (sortTypeName.equals("BuyPriceAscending")) {
+			Collections.sort(movies, new Comparator<Movie>() {
+				@Override
+				public int compare(Movie o1, Movie o2) {
+					return Double.compare(o1.getBuyPrice(), o2.getBuyPrice());
+				}
+			});
+		} else if (sortTypeName.equals("RentPriceDescending")) {
+			Collections.sort(movies, new Comparator<Movie>() {
+				@Override
+				public int compare(Movie o1, Movie o2) {
+					return Double.compare(o1.getRentPrice(), o2.getRentPrice());
+				}
+			});
+			Collections.reverse(movies);
+		} else if (sortTypeName.equals("RentPriceAscending")) {
+			Collections.sort(movies, new Comparator<Movie>() {
+				@Override
+				public int compare(Movie o1, Movie o2) {
+					return Double.compare(o1.getRentPrice(), o2.getRentPrice());
+				}
+			});
+		}
+		
+		return movies;
+	}
+
+	public List<Movie> getMoviesByPrice(double minD, double maxD) {
+		List<Movie> movies = new ArrayList<Movie>(getAllMovies());
+		List<Movie> sortedMovies = new ArrayList<Movie>();
+		for (Movie movie : movies ) {
+			if(movie.getBuyPrice() > minD && movie.getBuyPrice() < maxD) {
+				sortedMovies.add(movie);
+			}
+		}
+		return sortedMovies;
 	}
 }
